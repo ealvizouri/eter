00a28a0e48f97c8e3146905cdefec8076484101e
Mariano Alvizouri <ealvizouri@gmail.com>
2023-11-21 21:45:09 -0600

    included tailwindcss in front-end folder

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="h-screen flex justify-center items-center">
+      <header className="w-fit flex flex-col items-center gap-3">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-blue-700 hover:text-blue-900"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
