fb6951ead0ae28f746079341badf531826860f2c
chuyAguilar <jesusaguilar32265@gmail.com>
2023-11-27 13:59:24 -0600

    agregue el path para poder borrar productos

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -4,9 +4,10 @@ import { createProduct, getAllProducts,deleteProduct } from './handlers/products
 export default (app: any) => {
   const products = Router();
   products.use('/products', [
+    
+    products.delete('/',deleteProduct),
     products.get('/', getAllProducts),
-    products.post('/', createProduct),
-    products.delete('/',deleteProduct)
+    products.post('/', createProduct)
   ]);
   app.use('/v1', [products]);
 };
