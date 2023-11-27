68da8794558f3146a402400de204d5db212c7ef0
chuyAguilar <jesusaguilar32265@gmail.com>
2023-11-27 12:22:05 -0600

    modificacion de tabla productos para imagen

--- back-end/drizzle/migrations/0000_demonic_texas_twister.sql
+++ back-end/drizzle/migrations/0000_demonic_texas_twister.sql
@@ -2,7 +2,8 @@ CREATE TABLE `products` (
 	`id` text NOT NULL,
 	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	`active` integer DEFAULT 0 NOT NULL,
-	`name` text NOT NULL
+	`name` text NOT NULL,
+	'image'
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `products_id_unique` ON `products` (`id`);
