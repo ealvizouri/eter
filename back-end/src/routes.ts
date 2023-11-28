a9d77052fb878d1afb63db06f95b74356e02111e
chuyAguilar <jesusaguilar32265@gmail.com>
2023-11-28 13:11:11 -0600

    agregue la ruta para usuario con post para nuevo usuario

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express';
 import { createProduct, getAllProducts,updateProduct } from './handlers/products';
+import { createUser } from './handlers/users';
+
 
 export default (app: any) => {
+  const users = Router();
+  users.use('/usuarios',[
+    users.post('/',createUser),
+
+  ]);
+
   const products = Router();
   products.use('/products', [
     products.get('/', getAllProducts),
@@ -9,4 +17,6 @@ export default (app: any) => {
     products.put('/',updateProduct)
   ]);
   app.use('/v1', [products]);
+  app.use('/v1', [users]);
+
 };
