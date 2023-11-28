c9addab5775dd3e2c7ad838a4ea1201fdc614fa5
chuyAguilar <jesusaguilar32265@gmail.com>
2023-11-28 15:03:52 -0600

    agregue el query para verificar usuario'

--- back-end/src/handlers/users.ts
+++ back-end/src/handlers/users.ts
@@ -33,3 +33,33 @@ export const createUser = async (req, res, next) => {
       }
      
     };
+
+    //import jwt from 'jsonwebtoken';
+
+    // ...
+    
+    export const loginUser = async (req, res, next) => {
+      try {
+
+       const mail = req.body.mail
+       const password = req.body.password    
+        // Verificar las credenciales en la base de datos
+        const user = await db.select({mail: Users.mail, password: Users.password})
+        .from(Users).where(eq(Users.mail, mail));
+    
+        if (user.length === 0 || user[0].mail !== mail) {
+            return res.status(401).json({ error: 'Correo incorrecto' });
+          }else if(user[0].password !== password){
+            return res.status(401).json({ error: 'Contraseña incorrecta' });
+          }
+    
+        // Generar un token de sesión
+        //const token = jwt.sign({ userId: user.id }, 'token');
+    const token = mail + password + "tokenprovisional";
+        res.json({ token });
+      } catch (error) {
+        console.error('Error al iniciar sesión:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      }
+    };
+    
