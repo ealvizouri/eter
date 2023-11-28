80559c35f8e5df919c538b28e6871685f90bf819
chuyAguilar <jesusaguilar32265@gmail.com>
2023-11-28 13:44:27 -0600

    agregue la ruta para acceder a todos los usuarios

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { createProduct, getAllProducts,updateProduct } from './handlers/products';
-import { createUser } from './handlers/users';
+import { getAllUsers,createUser } from './handlers/users';
 
 
 export default (app: any) => {
   const users = Router();
   users.use('/usuarios',[
+    users.get('/',getAllUsers),
     users.post('/',createUser),
 
   ]);
