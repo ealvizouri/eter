bf19b55f5c382939b410de2db6c92d396b12733c
chuyAguilar <jesusaguilar32265@gmail.com>
2023-12-18 14:17:51 -0600

    ahora la contraseña se encripta y se compara

--- back-end/src/handlers/products.ts
+++ back-end/src/handlers/products.ts
@@ -3,6 +3,7 @@ import { db } from '../modules/drizzle';
 import uuid from '../modules/uuid';
 import { Products } from '../schema';
 
+
 export const getAllProducts = async (req, res, next) => {
   const allProducts = await db.select().from(Products);
 
@@ -11,37 +12,36 @@ export const getAllProducts = async (req, res, next) => {
   });
 };
 
+
 export const createProduct = async (req, res, next) => {
-  try{
-
-// Extraer archivo de imagen desde la solicitud
-const image = req.body.image;
-
-    // Almacena la imagen en el sistema de archivos (ajusta la ruta según tus necesidades)
-    const imagePath = `uploads/${uuid()}_${image}`;
-
-     const newProduct = await db
-    .insert(Products)
-    .values({
-      id: uuid(),
-      created_at: req.body.created_at,
-      quantity: req.body.quantity,
-      name: req.body.name,
-     image: imagePath // Guarda la ruta de la imagen en la base de datos
-
+  try {
+    // Extraer información del archivo de imagen desde la solicitud
+    const { filename, path } = req.file;
     
-    })
-    .returning({ id: Products.id, name: Products.name,created_at: Products.created_at,
-       quantity: Products.quantity,image: Products.image });
-  res.json({
-    data: newProduct,
-  });
-  }  catch (error) {
+
+    // Almacena la imagen en el sistema de archivos
+    const imagePath = `uploads/${uuid()}_${filename}`;
+
+    const newProduct = await db
+      .insert(Products)
+      .values({
+        id: uuid(),
+        created_at: req.body.created_at,
+        quantity: req.body.quantity,
+        name: req.body.name,
+        image: imagePath, // Guarda la ruta de la imagen en la base de datos
+      })
+      .returning({ id: Products.id, name: Products.name, created_at: Products.created_at, quantity: Products.quantity, image: Products.image });
+
+    res.json({
+      data: newProduct,
+    });
+  } catch (error) {
     console.error('Error al crear un nuevo producto:', error);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
- 
 };
+
 
 export const deleteProduct = async (req, res, next) =>{
 
