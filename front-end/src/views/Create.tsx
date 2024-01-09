7fcdf868841375f272ccd1eefffaedec3007a3f8
Aura Renata <perezugaldeaurarenata@gmail.com>
2024-01-08 20:21:10 -0600

    Create.tsx was create

--- front-end/src/views/Create.tsx
+++ front-end/src/views/Create.tsx
@@ -1,14 +1,20 @@
-import React from "react" 
+// Create.tsx
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Create = () => {
-    return(
-        <div className="text-center">
-            Create
-            Aqui va la creacion del producto
-        </div>
-    )
+  return (
+    <div className="p-4">
+      <Link
+        to={`/list`}
+        className="font-bold px-4 py-2 bg-red-500 text-white rounded-md"
+      >
+        Regresar al Listado
+      </Link>
 
-}
+      {/* Resto del contenido de tu formulario o componente Create */}
+    </div>
+  );
+};
 
-
-export default Create
+export default Create;
